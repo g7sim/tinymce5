ccea3bbe152b895aba6e5ef391609b90e510442a
manu37 <marinello@pixolution.ch>
2019-01-17 11:11:36 +0100

    remove TinyDrive Access

--- config/config.php
+++ config/config.php
@@ -4,6 +4,3 @@ $plugin_cf['tinymce5']['init']="tinymce5";
 $plugin_cf['tinymce5']['utf8_marker']="äöü";
 $plugin_cf['tinymce5']['CDN_src']="https://cloud.tinymce.com/5-testing/tinymce.min.js";
 $plugin_cf['tinymce5']['CDN']="true";
-$plugin_cf['tinymce5']['TinyDrive_JWT_private_key']="";
-$plugin_cf['tinymce5']['TinyDrive_JWT_subId']="localTest";
-$plugin_cf['tinymce5']['TinyDrive_JWT_Name']="Admin";
