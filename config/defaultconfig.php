6d78c0654e125659118edb94640ca554405dd463
manu37 <marinello@pixolution.ch>
2020-06-08 08:37:29 +0200

    Update defaultconfig.php
    set the CDN channel from testing to stable.

--- config/defaultconfig.php
+++ config/defaultconfig.php
@@ -1,5 +1,5 @@
 <?php
 $plugin_cf['tinymce5']['init']="full";
 $plugin_cf['tinymce5']['utf8_marker']="äöü";
-$plugin_cf['tinymce5']['CDN_src']="https://cloud.tinymce.com/5-testing/tinymce.min.js";
+$plugin_cf['tinymce5']['CDN_src']="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js";
 $plugin_cf['tinymce5']['CDN']="true";   //"" = locally installed, "true" = CDN Variant 
